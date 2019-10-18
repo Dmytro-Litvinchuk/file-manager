80294636949d516b43f053e2d62ec8c1865939f9
Dmytro-Litvinchuk <dimawaw11@gmail.com>
2019-10-18 14:02:15 +0300

    upload_progress

---
+++ progress.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+$key = ini_get("session.upload_progress.prefix") . "myForm";
+if (isset($_SESSION[$key])) {
+    $current = $_SESSION[$key]["bytes_processed"];
+    $total = $_SESSION[$key]["content_length"];
+    echo $current < $total ? ceil($current / $total * 100) : 100;
+} else {
+    echo 100;
+}
